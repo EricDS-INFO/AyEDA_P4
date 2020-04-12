1967ddbe429c5ce123c9397e37b1ef4bd520edcd
Eric DS <eric.d.s.info@gmail.com>
2020-04-12 19:43:36 +0100

    DNI test update #1: vector of 1K DNI's

--- tests/dni.test.hpp
+++ tests/dni.test.hpp
@@ -24,7 +24,7 @@ SCENARIO( "DNI is created as a random number of 8 digits" )
             std::cout << dni_2 << std::endl;
         }
     }
-    GIVEN( "Two DNI Objects with same seed" )
+    AND_GIVEN( "Two DNI Objects with same seed" )
     {
         dni dni_1(1);
         dni dni_2(1);
@@ -41,6 +41,37 @@ SCENARIO( "DNI is created as a random number of 8 digits" )
             std::cout << dni_2 << std::endl;
         }
     }
+    AND_GIVEN( "A vector of 1000 DNI's" )
+    {
+        std::vector<dni> dni_v;
+        for (int i = 0; i < 1000; i++)
+        {
+            dni aux;
+            dni_v.push_back(aux);
+        }
+
+        THEN( "They sould be all different" ){
+            for (int i = 0; i < dni_v.size(); i++ )
+            {
+                for (int j = 0; j < dni_v.size(); j++ )
+                {
+                    if (i != j)
+                    {
+                        REQUIRE(dni_v[i].value() !=
+                                dni_v[j].value());
+                
+                    }
+                }
+            }
+        }
+        AND_THEN (" They sould all be printed "){
+            for (int i = 0; i < dni_v.size(); i++ )
+            {
+            //    std::cout << dni_v[i] << "\n";
+            }
+        }
+    }
 }
+
 
 
