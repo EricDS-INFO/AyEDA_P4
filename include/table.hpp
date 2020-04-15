a21a5fd07cf927b9fce0d2a24a702ab315055260
Eric DS <eric.d.s.info@gmail.com>
2020-04-15 13:46:14 +0100

    Table basic tests

--- include/table.hpp
+++ include/table.hpp
@@ -1,7 +1,15 @@
 #pragma once
+
 #include "slot.hpp"
-#include "hash_func.hpp"
-#include "exp_func.hpp"
+// Hash functions
+#include "hash_func_add.hpp"
+#include "hash_func_mod.hpp"
+#include "hash_func_pse-rand.hpp"
+// Explore functions
+#include "exp_func_cuad.hpp"
+#include "exp_func_doub.hpp"
+#include "exp_func_lin.hpp"
+#include "exp_func_rehash.hpp"
 
 template<class KEY>
 class table
@@ -9,52 +17,68 @@ class table
 private:
     slot<KEY>** slots_;
 
-    exp_func<KEY>*   exploring_;
-    hash_func<KEY>*   hashing_;
+    exp_func<KEY>*   explorer_;
+    hash_func<KEY>*   hasher_;
 
-    int sz_;
+    int sz_, slt_sz_;
 
 public:
     table(int tb_sz, int sl_sz, int t_hash, int t_expl);
     ~table();
 
+    int size() { return sz_; }
+    int slot_size() { return slt_sz_; }
+    std::string hasher() const { return "Add"; } 
+    std::string explorer() const { return "Lin"; } 
 
 };
 
 
 
 template<class KEY>
-table<KEY>::table(int tb_sz, int sl_sz, int t_hash, int t_expl)
+table<KEY>::table(int tb_sz, int slt_sz, int t_hash, int t_expl)
 {
     sz_ = tb_sz;
+    slt_sz_ = slt_sz;
     slots_ = new  slot<KEY> *[sz_];
     for(int i = 0; i < sz_; i++)
     {
-        slots_[i] = new slot<KEY>(sl_sz);
+        slots_[i] = new slot<KEY>(slt_sz_);
     }
 
     switch(t_hash)
     {
         case 1:
+            hasher_ = new hash_func_mod<KEY>();
             break;
         case 2:
+            hasher_ = new hash_func_add<KEY>();
             break;
         case 3:
+            hasher_ = new hash_func_pseRand<KEY>();
             break;
         default:
+            hasher_ = new hash_func_mod<KEY>();
             break;
     }
     switch(t_expl)
     {
         case 1:
+            explorer_ = new exp_func_lin<KEY>();
             break;
         case 2:
+            explorer_ = new exp_func_cuad<KEY>();
             break;
         case 3:
+            explorer_ = new exp_func_doub<KEY>();
             break;
+
         case 4:
+            explorer_ = new exp_func_rehash<KEY>();
             break;
+
         default:
+            explorer_ = new exp_func_lin<KEY>();
             break;
     }
 }
--- tests/board.test.hpp
+++ tests/board.test.hpp
@@ -8,6 +8,43 @@ SCENARIO( "Testing the creation of a hash table", "[structure]" )
     {
         int table_sz = 5, slot_sz = 2, h_fun_t = 1, e_fun_t = 1;
         table<dni> first_table(table_sz, slot_sz, h_fun_t, e_fun_t);
-        
+
+        CHECK(table_sz > 0);
+        CHECK(slot_sz >= 1);
+        CHECK(h_fun_t >= 1); CHECK(h_fun_t <= 3);
+        CHECK(h_fun_t >= 1); CHECK(h_fun_t <= 4);
+        WHEN( "Asked for size" )
+        {
+            THEN( "Should provide the correct value" )
+            {
+                REQUIRE(first_table.size() == table_sz );
+            }
+        }
+        AND_WHEN( "Asked for the slots size" )
+        {
+
+            THEN( "Should provide the correct value" )
+            {
+                REQUIRE(first_table.slot_size() == slot_sz);
+            }
+        }
+
+        AND_WHEN( "Asked for the hash function" )
+        {
+
+            THEN( "Should provide the correct name" )
+            {
+                REQUIRE(first_table.hasher() == "Add");
+            }
+        }
+
+        AND_WHEN( "Asked for the exploration function" )
+        {
+
+            THEN( "Should provide the correct value" )
+            {
+                REQUIRE(first_table.explorer() == "Lin");
+            }
+        }
     }
 }
